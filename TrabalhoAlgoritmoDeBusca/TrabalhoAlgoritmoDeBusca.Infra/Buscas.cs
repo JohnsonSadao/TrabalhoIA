0fadcd5763e19d311a73a656c9ee6fde2eb92b63
johnson sadao <Johnson9697@hotmail.com>
2018-09-11 19:03:35 -0300

    Commit inicial

---
+++ TrabalhoAlgoritmoDeBusca/TrabalhoAlgoritmoDeBusca.Infra/Buscas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoAlgoritmoDeBusca.Infra
+{
+    public class Buscas
+    {
+        public Stack<string> caminho;
+
+        public Stack<string> BuscaProfundidadeLimitada(Vertice cidadeOrigem, Vertice cidadeDestino, int limite)
+        {
+            caminho.Clear();
+            caminho.Push(cidadeOrigem.Nome);
+            return;
+        }
+
+        public Stack<string> BPLRecursivo(Vertice atual, Vertice cidadeDestino, int limite)
+        {
+            if (ChecarObjetivo(atual, cidadeDestino))
+            {
+                caminho.Push(atual.Nome);
+                return caminho;
+            }
+            if (limite == 0)
+            {
+
+            }
+
+
+        }
+
+        public bool ChecarObjetivo(Vertice cidadeDestino, Vertice CidadeAtual)
+        {
+            if (cidadeDestino.Nome == CidadeAtual.Nome)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void BuscaProfundidade(Vertice cidade, Vertice cidadeDestino)
+        {
+            caminho.Push(cidade.Nome);
+            if (cidadeDestino.Nome != cidade.Nome)
+            {
+                BuscaProfundidade.(cidade.Neighbors.First, cidadeDestino);
+            }
+        }
+
+    }
+}
---
+++ TrabalhoAlgoritmoDeBusca/TrabalhoAlgoritmoDeBusca/Features/Vertice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoAlgoritmoDeBusca
+{
+    public class Vertice
+    {
+        public string Nome { get; set; }
+        public LinkedList<Vertice> Neighbors { get; set; }
+    }
+}
